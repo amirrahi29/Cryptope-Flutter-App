a1de1f9c54c9a0a026fbf80381c249a86fec5feb
Amir Rahi <amirrahi29@gmail.com>
2023-03-18 00:46:02 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         "/": (context) {
-          return EventDetailsPage();
+          return IntroOnBoardingPage();
         }
       },
     );
